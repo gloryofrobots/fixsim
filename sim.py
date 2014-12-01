fe029eb437f880d55855c9e799bbc8eb0714f399
alex birulia <oleksandr.birulia@trdata.com>
2014-12-01 18:48:00 +0200

    implement support for market data

--- sim.py
+++ sim.py
@@ -1,14 +1,125 @@
 import sys
-import time
-import quickfix as fix
+import quickfix
+import yaml
+from twisted.internet import task
+from twisted.internet import reactor
+import uuid
+
+class MarketDataError(quickfix.Exception):
+    pass
+
+class FixSimError(Exception):
+    pass
+
+class Subscription(object):
+    def __init__(self, symbol):
+        super(Subscription, self).__init__()
+        self.symbol = symbol
+        self.sessions = set()
+        self.snapshot = None
+
+    def hasSessions(self):
+        return len(self.sessions) > 0
+
+    def removeSession(self, sessionID):
+        self.sessions.remove(sessionID)
+
+    def addSession(self, sessionID):
+        self.sessions.add(sessionID)
+
+    def setSnapshot(self, snapshot):
+        self.snapshot = snapshot
+
+    def __iter__(self):
+        return self.sessions.__iter__()
+
+    def __repr__(self):
+        return "<Subscription %s>" % self.symbol
+
+    def __len__(self):
+        return self.sessions.__len__()
+
+class Quote(object):
+    def __init__(self, price, size):
+        super(Quote, self).__init__()
+
+        self.price = price
+        self.size = size
 
 
-class Application(fix.Application):
+class Snapshot(object):
+    def __init__(self):
+        super(Snapshot, self).__init__()
+        self.bids = []
+        self.asks = []
+
+    def addBid(self, price, size):
+        self.bids.append(Quote(price, size))
+
+    def addAsk(self, price, size):
+        self.asks.append(Quote(price, size))
+
+
+class Application(quickfix.Application):
     orderID = 0
     execID = 0
 
+    def __init__(self, config):
+        super(Application, self).__init__()
+        self.config = config
+        self.subscriptions = {}
+
+        #YOU CAN ADD OPTION FOR FIX VERSIONS HERE!
+        import quickfix44
+        self.fix_version = quickfix44
+
+        self.loop = task.LoopingCall(self.publishMarketData)
+        self.loop.start(1, True)
+
+        symbols = self.config['symbols']
+        for symbol in symbols:
+            name = symbol['name']
+            subscription = Subscription(name)
+            snapshot = Snapshot()
+            for quote in symbol['bid']:
+                snapshot.addBid(quote['price'], quote['size'])
+
+            for quote in symbol['ask']:
+                snapshot.addAsk(quote['price'], quote['size'])
+
+            subscription.setSnapshot(snapshot)
+            self.subscriptions[name] = subscription
+
+    def publishMarketData(self):
+        print "publishMarketData", self.subscriptions
+        for subscription in self.subscriptions.values():
+
+            if not subscription.hasSessions():
+                continue
+
+            message = self.fix_version.MarketDataSnapshotFullRefresh()
+            group = self.fix_version.MarketDataSnapshotFullRefresh().NoMDEntries()
+
+            snapshot = subscription.snapshot
+            for quote in snapshot.bids:
+                group.setField(quickfix.MDEntryType('0'))
+                group.setField(quickfix.MDEntryPx(quote.price))
+                group.setField(quickfix.MDEntrySize(quote.size))
+                group.setField(quickfix.QuoteEntryID(self.generateQuoteID()))
+                message.addGroup( group )
+
+            for quote in snapshot.asks:
+                group.setField(quickfix.MDEntryType('1'))
+                group.setField(quickfix.MDEntryPx(quote.price))
+                group.setField(quickfix.MDEntrySize(quote.size))
+                group.setField(quickfix.QuoteEntryID(self.generateQuoteID()))
+                message.addGroup( group )
+
+            for sessionID in subscription:
+                self.sendToTarget(message, sessionID)
+
     def onCreate(self, sessionID):
-        return
+        pass
 
     def onLogon(self, sessionID):
         return
@@ -25,56 +136,169 @@ class Application(fix.Application):
     def toApp(self, sessionID, message):
         return
 
-    def fromApp(self, message, sessionID):
-        beginString = fix.BeginString()
-        msgType = fix.MsgType()
-        message.getHeader().getField(beginString)
-        message.getHeader().getField(msgType)
+    def sendToTarget(self, message, sessionID):
+        print "SEND TO SESSION", sessionID
+        quickfix.Session.sendToTarget(message, sessionID)
 
-        symbol = fix.Symbol()
-        side = fix.Side()
-        ordType = fix.OrdType()
-        orderQty = fix.OrderQty()
-        price = fix.Price()
-        clOrdID = fix.ClOrdID()
+    def createMarketDataFullRefresh(self):
+        message = self.fix_version.MarketDataSnapshotFullRefresh()
+        group = self.fix_version.MarketDataSnapshotFullRefresh().NoMDEntries()
+
+        group.setField(quickfix.MDEntryType('0'))
+        group.setField(quickfix.MDEntryPx(12.32))
+        group.setField(quickfix.MDEntrySize(100))
+        group.setField(quickfix.OrderID("ORDERID"))
+        message.addGroup( group )
+
+        group.setField(quickfix.MDEntryType('1'))
+        group.setField(quickfix.MDEntryPx(12.32))
+        group.setField(quickfix.MDEntrySize(100))
+        group.setField(quickfix.OrderID("ORDERID"))
+        message.addGroup( group )
+        return message
+
+
+    def _parse(self, message):
+        noMDEntries = quickfix.NoMDEntries()
+        message.getField(noMDEntries)
+
+        group = self.fix_version.MarketDataSnapshotFullRefresh.NoMDEntries()
+        MDEntryType = quickfix.MDEntryType()
+        MDEntryPx = quickfix.MDEntryPx()
+        MDEntrySize = quickfix.MDEntrySize()
+        orderID = quickfix.OrderID()
+
+        message.getGroup(1, group)
+        group.getField(MDEntryType)
+        group.getField(MDEntryPx)
+        group.getField(MDEntrySize)
+        group.getField(orderID)
+
+        message.getGroup(2, group)
+        group.getField(MDEntryType)
+        group.getField(MDEntryPx)
+        group.getField(MDEntrySize)
+        group.getField(orderID)
+
+
+    def onMarketDataRequest(self, message, sessionID):
+        print 'onMarketDataRequest', message, sessionID
+        requestID = quickfix.MDReqID()
+        message.getField(requestID)
+        print "Request id ", requestID.getValue()
+
+        relatedSym = self.fix_version.MarketDataRequest.NoRelatedSym()
+        symbolFix = quickfix.Symbol()
+        product = quickfix.Product()
+
+        message.getGroup(1, relatedSym)
+        relatedSym.getField(symbolFix)
+        relatedSym.getField(product)
+        print "onMarketDataRequest symbol", symbolFix, product
+        if product.getValue() != quickfix.Product_CURRENCY:
+            self.sendMarketDataReject(requestID, " product.getValue() != quickfix.Product_CURRENCY:", sessionID)
+            return
+
+        #bid
+        entryType = self.fix_version.MarketDataRequest.NoMDEntryTypes()
+        message.getGroup(1, entryType)
+        #print "onMarketDataRequest bid", entryType
+
+        #ask
+        message.getGroup(2, entryType)
+        #print "onMarketDataRequest ask", entryType
+
+        symbol = symbolFix.getValue()
+
+        if not self.isSymbolSupported(symbol):
+            self.sendMarketDataReject(requestID, "Symbol %s does not supported" % symbol , sessionID)
+
+        self.addSessionToSubscription(symbol, sessionID)
+
+    def isSymbolSupported(self, symbol):
+        return symbol in self.subscriptions
+
+    def getSubscription(self, symbol):
+        return self.subscriptions[symbol]
+
+    def addSessionToSubscription(self, symbol, sessionID):
+        if not self.isSymbolSupported(symbol):
+            raise MarketDataError("Symbol %s does not supported" % symbol )
+
+        subscription = self.getSubscription(symbol)
+        subscription.addSession(sessionID)
+
+    def sendMarketDataReject(self, requestID, reason, sessionID):
+         reject = self.fix_version.MarketDataRequestReject()
+         reject.setField(requestID)
+         text = quickfix.Text(reason)
+         reject.setField(text)
+         self.sendToTarget(reject, sessionID)
+
+    def onNewOrderSingle(self, message, beginString, sessionID):
+        symbol = quickfix.Symbol()
+        side = quickfix.Side()
+        ordType = quickfix.OrdType()
+        orderQty = quickfix.OrderQty()
+        price = quickfix.Price()
+        clOrdID = quickfix.ClOrdID()
+        quoteID = quickfix.QuoteID()
 
         message.getField(ordType)
-        if ordType.getValue() != fix.OrdType_LIMIT:
-            raise fix.IncorrectTagValue(ordType.getField())
+        if ordType.getValue() != quickfix.OrdType_LIMIT:
+            raise quickfix.IncorrectTagValue(ordType.getField())
 
         message.getField(symbol)
         message.getField(side)
         message.getField(orderQty)
         message.getField(price)
         message.getField(clOrdID)
+        message.getField(quoteID)
 
-        executionReport = fix.Message()
+        executionReport = quickfix.Message()
         executionReport.getHeader().setField(beginString)
-        executionReport.getHeader().setField(fix.MsgType(fix.MsgType_ExecutionReport))
+        executionReport.getHeader().setField(quickfix.MsgType(quickfix.MsgType_ExecutionReport))
 
-        executionReport.setField(fix.OrderID(self.genOrderID()))
-        executionReport.setField(fix.ExecID(self.genExecID()))
-        executionReport.setField(fix.OrdStatus(fix.OrdStatus_FILLED))
+        executionReport.setField(quickfix.OrderID(self.genOrderID()))
+        executionReport.setField(quickfix.ExecID(self.genExecID()))
+        executionReport.setField(quickfix.OrdStatus(quickfix.OrdStatus_FILLED))
         executionReport.setField(symbol)
         executionReport.setField(side)
-        executionReport.setField(fix.CumQty(orderQty.getValue()))
-        executionReport.setField(fix.AvgPx(price.getValue()))
-        executionReport.setField(fix.LastShares(orderQty.getValue()))
-        executionReport.setField(fix.LastPx(price.getValue()))
+        executionReport.setField(quickfix.CumQty(orderQty.getValue()))
+        executionReport.setField(quickfix.AvgPx(price.getValue()))
+        executionReport.setField(quickfix.LastShares(orderQty.getValue()))
+        executionReport.setField(quickfix.LastPx(price.getValue()))
         executionReport.setField(clOrdID)
         executionReport.setField(orderQty)
 
-        if beginString.getValue() == fix.BeginString_FIX40 or beginString.getValue() == fix.BeginString_FIX41 or beginString.getValue() == fix.BeginString_FIX42:
-            executionReport.setField(fix.ExecTransType(fix.ExecTransType_NEW))
+        if beginString.getValue() == quickfix.BeginString_FIX40 \
+                or beginString.getValue() == quickfix.BeginString_FIX41 \
+                or beginString.getValue() == quickfix.BeginString_FIX42:
+            executionReport.setField(quickfix.ExecTransType(quickfix.ExecTransType_NEW))
 
-        if beginString.getValue() >= fix.BeginString_FIX41:
-            executionReport.setField(fix.ExecType(fix.ExecType_FILL))
-            executionReport.setField(fix.LeavesQty(0))
+        if beginString.getValue() >= quickfix.BeginString_FIX41:
+            executionReport.setField(quickfix.ExecType(quickfix.ExecType_FILL))
+            executionReport.setField(quickfix.LeavesQty(0))
 
         try:
-            fix.Session.sendToTarget(executionReport, sessionID)
-        except fix.SessionNotFound as e:
+            self.sendToTarget(executionReport, sessionID)
+        except quickfix.SessionNotFound as e:
             return
+
+    def fromApp(self, message, sessionID):
+        fixMsgType = quickfix.MsgType()
+        beginString = quickfix.BeginString()
+        message.getHeader().getField(beginString)
+        message.getHeader().getField(fixMsgType)
+
+        print fixMsgType
+
+        msgType = fixMsgType.getValue()
+        print "MESSAGE TYPE:", msgType
+        if msgType == 'D':
+            self.onNewOrderSingle(message, beginString, sessionID)
+        elif msgType == 'V':
+            self.onMarketDataRequest(message, sessionID)
 
     def genOrderID(self):
         self.orderID += 1
@@ -84,18 +308,27 @@ class Application(fix.Application):
         self.execID += 1
         return self.execID
 
+    def generateQuoteID(self):
+        return str(uuid.uuid4())
 
+def load_simulator_config(path):
+    with open(path,'r') as stream:
+        cfg = yaml.load(stream)
+
+    return cfg
 try:
-    f = sys.argv[1]
-    settings = fix.SessionSettings(f)
-    application = Application()
-    storeFactory = fix.FileStoreFactory(settings)
-    logFactory = fix.ScreenLogFactory(settings)
-    acceptor = fix.SocketAcceptor(application, storeFactory, settings, logFactory)
+    fix_config = sys.argv[1]
+    settings = quickfix.SessionSettings(fix_config)
+
+    simulator_config = load_simulator_config(sys.argv[2])
+
+    application = Application(simulator_config)
+    storeFactory = quickfix.FileStoreFactory(settings)
+    logFactory = quickfix.ScreenLogFactory(settings)
+    acceptor = quickfix.SocketAcceptor(application, storeFactory, settings, logFactory)
     acceptor.start()
 
-    while 1:
-        time.sleep(1)
-except (fix.ConfigError, fix.RuntimeError) as e:
+    reactor.run()
+except (quickfix.ConfigError, quickfix.RuntimeError) as e:
     print(e)
 
